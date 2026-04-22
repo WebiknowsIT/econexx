997271c8a4695b686a41291ab88768f0095c514f
MonuKumarGautam <167089006+MonuKumarGautam@users.noreply.github.com>
2026-04-22 17:47:43 +0530

    sdfsd

--- app/(dashboard)/change-password/page.jsx
+++ app/(dashboard)/change-password/page.jsx
@@ -1,24 +1,13 @@
-import Breadcumb from "@/components/common/Breadcumb";
-import ChangePassword from "@/components/dashboard/ChangePassword";
-import Profile from "@/components/dashboard/Profile";
-import Footer1 from "@/components/footers/Footer";
-import Header from "@/components/headers/Header";
-import Topbar from "@/components/headers/Topbar";
-
 import React from "react";
 
 export const metadata = {
-  title: "Accout Address || Vineta - Multipurpose React Nextjs eCommerce",
-  description: "Vineta - Multipurpose React Nextjs eCommerce",
+  title: "Accout Address",
+  description: "",
 };
 export default function page() {
   return (
     <>
-      <Topbar parentClass="tf-topbar bg-dark-5 topbar-bg" />
-      <Header />
-      <Breadcumb pageName="Change Password" pageTitle="Change Password" />
-      <ChangePassword />
-      <Footer1 />
+      <h1>change password</h1>
     </>
   );
 }
